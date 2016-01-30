18501ffd46cd4bbb627760f2acc7a572a985ae54
Dustin Clinard <dustin.clinard@realartusa.com>
2016-01-30 12:54:18 -0500

    no message

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,16 +9,26 @@ public class GameController : MonoBehaviour {
     
 	// Use this for initialization
 	void Start () {
-	
+	   startGame();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	   
 	}
+    
+    void startGame(){
+         nextRound();
+    }
+    
+    void nextRound(){
+        currentDay += 1;
+        setDayText();
+        // load next level
+        
+    }
     
     void setDayText(){
         dayText.text = "Day " + currentDay;
-    
     }
 }
