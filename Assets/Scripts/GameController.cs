d7b605e01cc03f2f07db180fd18099baebc18061
Dustin Clinard <dustin.clinard@realartusa.com>
2016-01-30 12:49:13 -0500

    no message

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Text dayText;
+    int currentDay;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void setDayText(){
+        dayText.text = "Day " + currentDay;
+    
+    }
+}
