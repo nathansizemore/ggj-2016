ba44b42320f991b41df85021c56ba5b7bb2de46c
Dustin Clinard <dustin.clinard@realartusa.com>
2016-01-30 13:24:12 -0500

    no message

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
+  
 public class GameController : MonoBehaviour {
 
     public Text dayText;
     int currentDay;
+    Scene activeLevelScene;
+    
     
 	// Use this for initialization
 	void Start () {
@@ -14,7 +17,9 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	   
+	   if (Input.GetKeyDown(KeyCode.Space)){
+           nextRound();
+       }
 	}
     
     void startGame(){
@@ -25,10 +30,21 @@ public class GameController : MonoBehaviour {
         currentDay += 1;
         setDayText();
         // load next level
-        
+        StartCoroutine(LoadNextLevel());
     }
     
     void setDayText(){
         dayText.text = "Day " + currentDay;
     }
+    
+    IEnumerator LoadNextLevel() {
+        AsyncOperation async = SceneManager.LoadSceneAsync("Level" + currentDay, LoadSceneMode.Additive);
+        yield return async;
+        if (activeLevelScene != null){
+            SceneManager.UnloadScene(activeLevelScene.name);
+        }
+       activeLevelScene = SceneManager.GetSceneAt(1);
+       
+    }
+    
 }
