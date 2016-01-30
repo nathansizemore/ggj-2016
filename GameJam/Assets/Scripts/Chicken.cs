d4c0f56bea862b84ab42e9be39f0ffa6a027ec25
Nathan Sizemore <nathanrsizemore@gmail.com>
2016-01-30 12:13:59 -0500

    VCCode things

--- GameJam/Assets/Scripts/Chicken.cs
+++ GameJam/Assets/Scripts/Chicken.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class Chicken : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start() 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update() 
+	{
+		if (Input.GetKeyDown(KeyCode.W)) 
+		{
+			
+		}
 	}
 }
