ebb67caf6441f52291fce0d1fbfbd16d9e4fa0ba
Nathan Sizemore <nathanrsizemore@gmail.com>
2016-01-30 16:46:04 -0500

    Chicken sprite swap complete

--- Assets/Scripts/Chicken.cs
+++ Assets/Scripts/Chicken.cs
@@ -3,12 +3,20 @@
 public class Chicken : MonoBehaviour {
 
 	public float ChickenSpeed = 20.5f;
+    
+    public SpriteRenderer UpSprite;
+    public SpriteRenderer DownSprite;
+    public SpriteRenderer LeftSprite;
+    public SpriteRenderer RightSprite;
+   
 	GameController gc;
     
-	void Start(){
+	void Start()
+    {
         gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
-       
+        SwapSpriteUp();
     }
+    
 	void Update() 
 	{
 		if (Input.GetKey(KeyCode.W)) 
@@ -28,38 +36,44 @@ public class Chicken : MonoBehaviour {
             MoveChickenRight();
         }
         
-        if (Input.GetButtonDown("Fire1")){
+        if (Input.GetButtonDown("Fire1"))
+        {
             usePowerup();
         }
 	}
     
     private void MoveChickenUp()
     {
+        SwapSpriteUp();
         this.transform.rotation = Quaternion.Euler(0, 0, 0);
         this.transform.position += this.transform.forward * Time.deltaTime * ChickenSpeed;
     }
     
     private void MoveChickenDown()
     {
+        SwapSpriteDown();
         this.transform.rotation = Quaternion.Euler(0, 180, 0);
         this.transform.position += this.transform.forward * Time.deltaTime * ChickenSpeed;
     }
     
     private void MoveChickenLeft()
     {
+        SwapSpriteLeft();
         this.transform.rotation = Quaternion.Euler(0, 270, 0);
         this.transform.position += this.transform.forward * Time.deltaTime * ChickenSpeed;
     }
     
     private void MoveChickenRight()
     {
+        SwapSpriteRight();
         this.transform.rotation = Quaternion.Euler(0, 90, 0);
         this.transform.position += this.transform.forward * Time.deltaTime * ChickenSpeed;
     }
     
       
     void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == "Powerup"){
+        if (other.gameObject.tag == "Powerup")
+        {
              gc.setQueuedPowerup(other.gameObject.GetComponent<Powerup>());
         }
         Destroy(other.gameObject);
@@ -74,4 +88,39 @@ public class Chicken : MonoBehaviour {
         
     }
     
+    private void SwapSpriteUp()
+    {
+        UpSprite.gameObject.SetActive(true);
+        
+        DownSprite.gameObject.SetActive(false);
+        LeftSprite.gameObject.SetActive(false);
+        RightSprite.gameObject.SetActive(false);
+    }
+    
+    private void SwapSpriteDown()
+    {
+        DownSprite.gameObject.SetActive(true);
+        
+        UpSprite.gameObject.SetActive(false);        
+        LeftSprite.gameObject.SetActive(false);
+        RightSprite.gameObject.SetActive(false);
+    }
+    
+    private void SwapSpriteLeft()
+    {
+        LeftSprite.gameObject.SetActive(true);
+        
+        UpSprite.gameObject.SetActive(false);
+        DownSprite.gameObject.SetActive(false);        
+        RightSprite.gameObject.SetActive(false);
+    }
+    
+    private void SwapSpriteRight()
+    {
+        RightSprite.gameObject.SetActive(true);
+        
+        UpSprite.gameObject.SetActive(false);
+        DownSprite.gameObject.SetActive(false);
+        LeftSprite.gameObject.SetActive(false);
+    }
 }
